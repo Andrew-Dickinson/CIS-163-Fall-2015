510d3aa9fcd8be2a303356703defefcb0fceee17
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-09-05 05:46:44 -0400

    Added tests and other methods

--- src/countdowntimer/CountDownTimer.java
+++ src/countdowntimer/CountDownTimer.java
@@ -73,13 +73,15 @@ public class CountDownTimer {
         } else if (timeData.length == 2){
             constructTimer(0, Integer.parseInt(timeData[0]), Integer.parseInt(timeData[1]));
         } else if (timeData.length == 1){
-            constructTimer(0, 0, Integer.parseInt(timeData[0]));
-        } else {
             if (timeString.length() == 0){
                 //String is empty
                 constructTimer(0, 0, 0);
-            } else {
-                //The string doesn't fit
+                return; //To avoid throwing an exception
+            }
+
+            try {
+                constructTimer(0, 0, Integer.parseInt(timeData[0]));
+            } catch (NumberFormatException e){
                 throw new IllegalArgumentException();
             }
         }
@@ -93,6 +95,16 @@ public class CountDownTimer {
      */
     public static boolean equals(CountDownTimer c1, CountDownTimer c2){
         return c1.equals(c2);
+    }
+
+    /**
+     * Compares c1 to c2
+     * @param c1 The first CountDownTimer
+     * @param c2 The second CountDownTimer
+     * @return 1 if c1 is greater than c2, -1 if c1 is less than c2, and 0 if they're equal
+     */
+    public static int compareTo(CountDownTimer c1, CountDownTimer c2){
+        return c1.compareTo(c2);
     }
 
     /**
@@ -126,13 +138,121 @@ public class CountDownTimer {
         }
     }
 
-    //TODO Finish method
+    /**
+     * Compares this to other
+     * @param other The other CountDownObject to compare to
+     * @return 1 if this is greater than other, -1 if this is less than other, and 0 if they're equal
+     */
     public int compareTo(CountDownTimer other){
         if (equals(other)){
             return 0;
         } else {
-
+            int tempThisSeconds = getOverallSeconds();
+            int tempOtherSeconds = other.getOverallSeconds();
+            if (tempOtherSeconds < tempThisSeconds){
+                //This is greater
+                return 1;
+            } else {
+                //This is less
+                return -1;
+            }
         }
+    }
+
+    /**
+     * Returns a string version of the timer data
+     * @return A string in the format hh:mm:ss
+     */
+    public String toString(){
+        String hourString = "" + hours;
+        String minuteString = "" + minutes;
+        String secondString = "" + seconds;
+        if (hours < 10){
+            hourString = "0" + hourString;
+        }
+        if (minutes < 10){
+            minuteString = "0" + minuteString;
+        }
+        if (seconds < 10){
+            secondString = "0" + secondString;
+        }
+        return hourString + ":" + minuteString + ":" + secondString;
+    }
+
+    /**
+     * Adds or subtracts the indicated amount in seconds
+     * @param amount The amount in seconds. Negative to subtract. Positive to add
+     * @throws IllegalArgumentException If subtraction goes to less than zero
+     */
+    private void modify(int amount){
+        int tempOverallSeconds = getOverallSeconds();
+        tempOverallSeconds += amount;
+        if (tempOverallSeconds < 0){
+            throw new IllegalArgumentException();
+        }
+
+        hours = (tempOverallSeconds / 3600); //Integers are rounded down automatically
+        minutes = tempOverallSeconds % 3600;
+        seconds = tempOverallSeconds % 60;
+
+    }
+
+    /**
+     * Subtracts the specified amount
+     * @param amount The amount to subtract
+     * @throws IllegalArgumentException If amount < 0 or amount > getOverallSeconds()
+     */
+    public void subtract(int amount){
+        if (amount < 0 || amount > getOverallSeconds()){
+            throw new IllegalArgumentException();
+        }
+        modify(-amount);
+    }
+
+    /**
+     * Subtracts the other CountDownTimer from this one
+     * @param other The CountDownTimer to subtract based on
+     */
+    public void subtract(CountDownTimer other){
+        subtract(other.getOverallSeconds());
+    }
+
+    /**
+     * Adds the specified amount to the timer
+     * @param amount The amount to add
+     */
+    public void add(int amount){
+        if (amount < 0) {
+            throw new IllegalArgumentException();
+        }
+        modify(amount);
+    }
+
+    /**
+     * Adds the other CountDownTimer to this one
+     * @param other The CountDownTimer to add based on
+     */
+    public void add(CountDownTimer other){
+        add(other.getOverallSeconds());
+    }
+
+
+    /**
+     * Subtracts 1 from the timer
+     */
+    public void dec(){
+        if (getOverallSeconds() > 0){
+            subtract(1);
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * Adds 1 to the timer
+     */
+    public void inc(){
+        add(1);
     }
 
     public int getHours() {
@@ -157,5 +277,13 @@ public class CountDownTimer {
 
     public void setSeconds(int seconds) {
         this.seconds = seconds;
+    }
+
+    /**
+     * Gets the total value of the timer in seconds
+     * @return The total value in seconds
+     */
+    public int getOverallSeconds(){
+        return hours * 3600 + minutes * 60 + seconds;
     }
 }
---
+++ src/countdowntimer/CountDownTimerTest.java
@@ -0,0 +1,242 @@
+package countdowntimer;
+
+import org.junit.Test;
+
+import java.util.IllegalFormatCodePointException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andrew on 9/5/15.
+ */
+public class CountDownTimerTest {
+
+    @Test
+    public void testDefaultConstructor(){
+        CountDownTimer def = new CountDownTimer();
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 0 && def.getSeconds() == 0);
+    }
+
+    @Test
+    public void testDefaultConstructorWithSecondsValid(){
+        CountDownTimer def = new CountDownTimer(24);
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 0 && def.getSeconds() == 24);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDefaultConstructorWithSecondsNegative(){
+        CountDownTimer def = new CountDownTimer(-4);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDefaultConstructorWithSecondsHigh(){
+        CountDownTimer def = new CountDownTimer(67);
+    }
+
+    @Test
+    public void testDefaultConstructorWithMinutesValid(){
+        CountDownTimer def = new CountDownTimer(24, 0);
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 24 && def.getSeconds() == 0);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDefaultConstructorWithMinutesNegative(){
+        CountDownTimer def = new CountDownTimer(-4, 0);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDefaultConstructorWithMinutesHigh(){
+        CountDownTimer def = new CountDownTimer(67, 0);
+    }
+
+    @Test
+    public void testDefaultConstructorWithHoursValid(){
+        CountDownTimer def = new CountDownTimer(16, 0, 0);
+        assertTrue(def.getHours() == 16 && def.getMinutes() == 0 && def.getSeconds() == 0);
+    }
+
+    @Test
+    public void testDefaultConstructorWithHoursValidAll(){
+        CountDownTimer def = new CountDownTimer(4, 33, 15);
+        assertTrue(def.getHours() == 4 && def.getMinutes() == 33 && def.getSeconds() == 15);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDefaultConstructorWithHoursNegative() {
+        CountDownTimer def = new CountDownTimer(-4, 0, 0);
+    }
+
+    @Test
+    public void testOtherClassConstructor(){
+        CountDownTimer def1 = new CountDownTimer(38, 34, 53);
+        CountDownTimer def2 = new CountDownTimer(def1);
+        assertTrue(def1.getHours() == def2.getHours() &&
+                   def1.getMinutes() == def2.getMinutes() &&
+                   def1.getSeconds() == def2.getSeconds());
+    }
+
+    @Test
+    public void testStringConstructorHMS(){
+        CountDownTimer def = new CountDownTimer("03:45:23");
+        assertTrue(def.getHours() == 3 && def.getMinutes() == 45 && def.getSeconds() == 23);
+    }
+
+    @Test
+    public void testStringConstructorMS(){
+        CountDownTimer def = new CountDownTimer("45:23");
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 45 && def.getSeconds() == 23);
+    }
+
+    @Test
+    public void testStringConstructorS(){
+        CountDownTimer def = new CountDownTimer("23");
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 0 && def.getSeconds() == 23);
+    }
+
+    @Test
+    public void testStringConstructorEmpty(){
+        CountDownTimer def = new CountDownTimer("");
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 0 && def.getSeconds() == 0);
+    }
+
+    @Test(expected= IllegalArgumentException.class)
+    public void testStringConstructorInvalid(){
+        CountDownTimer def = new CountDownTimer("sdafasldfj");
+    }
+
+    @Test
+    public void testStaticEquals(){
+        CountDownTimer def1 = new CountDownTimer(15, 43, 24);
+        CountDownTimer def2 = new CountDownTimer(def1);
+        assertTrue(CountDownTimer.equals(def1, def2));
+    }
+
+    @Test
+    public void testInstanceEquals(){
+        CountDownTimer def1 = new CountDownTimer(7, 4, 56);
+        CountDownTimer def2 = new CountDownTimer(def1);
+        assertTrue(def1.equals(def2));
+    }
+
+    @Test
+    public void testInstanceCompareToEquals(){
+        CountDownTimer def1 = new CountDownTimer(7, 4, 56);
+        CountDownTimer def2 = new CountDownTimer(def1);
+        assertTrue(def1.compareTo(def2) == 0);
+    }
+
+    @Test
+    public void testInstanceCompareToLess(){
+        CountDownTimer def1 = new CountDownTimer(7, 4, 56);
+        CountDownTimer def2 = new CountDownTimer(7, 4, 57);
+        assertTrue(def1.compareTo(def2) == -1);
+    }
+
+    @Test
+    public void testInstanceCompareToGreater(){
+        CountDownTimer def1 = new CountDownTimer(7, 5, 56);
+        CountDownTimer def2 = new CountDownTimer(7, 4, 57);
+        assertTrue(def1.compareTo(def2) == 1);
+    }
+
+    @Test
+    public void testStaticCompareToEquals(){
+        CountDownTimer def1 = new CountDownTimer(7, 4, 56);
+        CountDownTimer def2 = new CountDownTimer(def1);
+        assertTrue(CountDownTimer.compareTo(def1, def2) == 0);
+    }
+
+    @Test
+    public void testStaticCompareToLess(){
+        CountDownTimer def1 = new CountDownTimer(7, 4, 56);
+        CountDownTimer def2 = new CountDownTimer(7, 4, 57);
+        assertTrue(CountDownTimer.compareTo(def1, def2) == -1);
+    }
+
+    @Test
+    public void testStaticCompareToGreater(){
+        CountDownTimer def1 = new CountDownTimer(7, 5, 56);
+        CountDownTimer def2 = new CountDownTimer(7, 4, 57);
+        assertTrue(CountDownTimer.compareTo(def1, def2) == 1);
+    }
+
+    @Test
+    public void testToString(){
+        CountDownTimer def = new CountDownTimer(4, 20, 7);
+        assertTrue(def.toString().equals("04:20:07"));
+    }
+
+    @Test
+    public void testSubtractValid(){
+        CountDownTimer def = new CountDownTimer(4, 33, 15);
+        def.subtract(17);
+        assertTrue(def.getHours() == 4 && def.getMinutes() == 32 && def.getSeconds() == 58);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSubtractNegative(){
+        CountDownTimer def = new CountDownTimer(4, 33, 15);
+        def.subtract(-17);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testSubtractTooFar(){
+        CountDownTimer def = new CountDownTimer(0, 1, 15);
+        def.subtract(200);
+    }
+
+    @Test
+    public void testSubtractClassValid(){
+        CountDownTimer def1 = new CountDownTimer(4, 33, 15);
+        CountDownTimer def2 = new CountDownTimer(0, 3, 20);
+        def1.subtract(def2);
+        assertTrue(def1.getHours() == 4 && def1.getMinutes() == 29 && def1.getSeconds() == 55);
+    }
+
+    @Test
+    public void testDecValid(){
+        CountDownTimer def = new CountDownTimer(0, 1, 15);
+        def.dec();
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 1 && def.getSeconds() == 14);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testDecInValid(){
+        CountDownTimer def = new CountDownTimer(0, 0, 0);
+        def.dec();
+    }
+
+    @Test
+    public void testAddValid(){
+        CountDownTimer def = new CountDownTimer(4, 33, 15);
+        def.add(56);
+        assertTrue(def.getHours() == 4 && def.getMinutes() == 34 && def.getSeconds() == 11);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testAddNegative(){
+        CountDownTimer def = new CountDownTimer(4, 33, 15);
+        def.add(-17);
+    }
+
+    @Test
+    public void testAddClassValid(){
+        CountDownTimer def1 = new CountDownTimer(4, 33, 15);
+        CountDownTimer def2 = new CountDownTimer(0, 3, 20);
+        def1.add(def2);
+        assertTrue(def1.getHours() == 4 && def1.getMinutes() == 36 && def1.getSeconds() == 35);
+    }
+
+    @Test
+    public void testIncValid(){
+        CountDownTimer def = new CountDownTimer(0, 1, 15);
+        def.inc();
+        assertTrue(def.getHours() == 0 && def.getMinutes() == 1 && def.getSeconds() == 16);
+    }
+
+    @Test
+    public void testGetOverallSeconds(){
+        CountDownTimer def1 = new CountDownTimer(15, 43, 24);
+        assertTrue(def1.getOverallSeconds() == 15*3600 + 43*60 + 24);
+    }
+}
