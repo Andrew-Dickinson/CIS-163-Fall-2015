acbf62758e13805651c1272e0ca617ec04423460
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-09-11 16:24:45 -0400

    Created JPanel and driver husk

---
+++ src/countdowntimer/CDTPanel.java
@@ -0,0 +1,11 @@
+package countdowntimer;
+
+import javax.swing.*;
+
+/**
+ * Created by Andrew on 9/11/15.
+ */
+public class CDTPanel extends JPanel {
+
+
+}
---
+++ src/countdowntimer/CountDownTimerDriver.java
@@ -0,0 +1,11 @@
+package countdowntimer;
+
+/**
+ * Created by Andrew on 9/5/15.
+ */
+public class CountDownTimerDriver {
+    public static void main(String[] args){
+        //TODO Lame test version that no one uses
+        //TODO Java style guide
+    }
+}
