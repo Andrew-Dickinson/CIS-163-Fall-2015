a8670824b3bceddf39b6fc3ea70b51d526158b93
Andrew Dickinson <fortknoxcomputer@gmail.com>
2015-09-15 20:39:46 -0400

    Added "tests" to main test method file

--- src/countdowntimer/CountDownTimerMainMethodTests.java
+++ src/countdowntimer/CountDownTimerMainMethodTests.java
@@ -6,6 +6,28 @@ package countdowntimer;
  **********************************************************************/
 public class CountDownTimerMainMethodTests {
     public static void main(String[] args){
+        CountDownTimer s = new CountDownTimer("2:59:8");
+        System.out.println("Time: " + s);
 
+        s = new CountDownTimer("20:8");
+        System.out.println("Time: " + s);
+
+        s = new CountDownTimer("8");
+        System.out.println("Time: " + s);
+
+        CountDownTimer s1 = new CountDownTimer(25, 2, 20);
+        System.out.println("Time: " + s1);
+
+        s1.subtract(1000);
+        System.out.println("Time: " + s1);
+
+        s1.add(1000);
+        System.out.println("Time: " + s1);
+
+        CountDownTimer s2 = new CountDownTimer(40, 10, 20);
+        s2.subtract(100);
+        for (int i = 0; i < 4000; i++)
+            s2.dec();
+        System.out.println("Time: " + s2);
     }
 }
