3b6f291d73fc14134bb0894688e8136b0738d58d
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-09-15 20:36:51 -0400

    Added main method tests class

---
+++ src/countdowntimer/CountDownTimerMainMethodTests.java
@@ -0,0 +1,11 @@
+package countdowntimer;
+
+/***********************************************************************
+ * Prints the output from a couple constructors. Is a poor test system
+ * Created by Andrew on 9/5/15.
+ **********************************************************************/
+public class CountDownTimerMainMethodTests {
+    public static void main(String[] args){
+
+    }
+}
