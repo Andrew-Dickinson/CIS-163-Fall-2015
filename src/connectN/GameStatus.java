f3c1af4df877b6c2935b033cc5a4dcb822df00aa
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-09-22 13:20:12 -0400

    Completed step 2: Add enum for gamestatus

--- src/connectN/GameStatus.java
+++ src/connectN/GameStatus.java
@@ -1,8 +1,24 @@
 package connectN;
 
 /***********************************************************************
+ * The four states that the game can be in
  * Created by Andrew on 9/22/15.
  **********************************************************************/
 public enum GameStatus {
-
+    /**
+     * Player 1 is the winner. Game is finished
+     */
+    Player1Won,
+    /**
+     * Player 2 is the winner. Game is finished
+     */
+    Player2Won,
+    /**
+     * There is a tie. Game is finished
+     */
+    Cats,
+    /**
+     * The game is still in progress
+     */
+    InProgress
 }
