e1a611bb93c0460309f500e0b8a1439d2c5ee7a7
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2015-11-21 20:33:13 -0500

    Added Primary GUI

--- src/us/andrewdickinson/gvsu/CIS163/linkedMessages/Driver.java
+++ src/us/andrewdickinson/gvsu/CIS163/linkedMessages/Driver.java
@@ -1,11 +1,25 @@
 package us.andrewdickinson.gvsu.CIS163.linkedMessages;
 
+import javax.swing.*;
+
 /***********************************************************************
  * Run the program for user interaction
  * Created by Andrew on 11/9/15.
  **********************************************************************/
 public class Driver {
     public static void main(String[] args){
+        //Create the frame and add a menu bar
+        JFrame frame = new JFrame("Message Scrambler/De-scrambler");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel panel = new MessagePanel(new ScrambledMessage("Andrew"));
+
+        //Add the panel to the frame
+        frame.getContentPane().add(panel);
+
+        //Configure the frame
+        frame.setSize(600, 250);
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
---
+++ src/us/andrewdickinson/gvsu/CIS163/linkedMessages/MessagePanel.java
@@ -0,0 +1,196 @@
+package us.andrewdickinson.gvsu.CIS163.linkedMessages;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/***********************************************************************
+ * Created by Andrew on 11/21/15.
+ **********************************************************************/
+public class MessagePanel extends JPanel {
+    /**
+     * The GridPanel that displays the characters
+     */
+    private JPanel characterGridPanel;
+
+    /**
+     * The message displayed by this panel
+     */
+    private ScrambledMessage message;
+
+    /**
+     * The simple message action buttons
+     */
+    private JButton insertCharacterButton;
+    private JButton removeCharacterButton;
+    private JButton replaceCharacterButton;
+    private JButton swapCharactersButton;
+
+    /**
+     * The random message action buttons
+     */
+    private JButton insertRandomCharacterButton;
+    private JButton removeRandomCharacterButton;
+    private JButton replaceRandomCharacterButton;
+    private JButton swapRandomCharactersButton;
+
+    public MessagePanel(ScrambledMessage message){
+        this.message = message;
+
+        ButtonListener buttonListener = new ButtonListener();
+
+        this.setLayout(new BorderLayout());
+
+        //Initialize and fill the characterGridPanel
+        updateCharacterGridPanel(message);
+
+        JPanel actionsPanel = new JPanel(new BorderLayout(10, 10));
+        actionsPanel.setSize(new Dimension(300, 300));
+
+        //Set up the simple (non-random) buttons
+        actionsPanel.add(
+                setupSimpleButtons(buttonListener),
+                BorderLayout.CENTER
+        );
+
+        //Set up the random buttons
+        actionsPanel.add(
+                setupRandomButtons(buttonListener),
+                BorderLayout.EAST
+        );
+
+        JPanel container = new JPanel();
+        container.add(actionsPanel);
+        add(container, BorderLayout.SOUTH);
+    }
+
+    /*******************************************************************
+     * Initializes the characterGridPanel based on a certain message
+     * @param message The message to base the grid panel on
+     ******************************************************************/
+    private void updateCharacterGridPanel(ScrambledMessage message){
+        if (characterGridPanel != null)
+            remove(characterGridPanel);
+
+        JPanel subPanel = new JPanel(
+                new GridLayout(2, message.length(), 10, 10)
+        );
+
+        for (int i = 0; i < message.length(); i++){
+            JLabel label = new JLabel(Integer.toString(i));
+            label.setFont(new Font("Arial", Font.PLAIN, 20));
+            subPanel.add(label);
+        }
+
+        for (int i = 0; i < message.length(); i++){
+            JLabel label = new JLabel(message.getCharacter(i).toString());
+            label.setFont(new Font("Arial", Font.PLAIN, 20));
+            subPanel.add(label);
+        }
+
+        characterGridPanel = new JPanel();
+        characterGridPanel.add(subPanel);
+        add(characterGridPanel, BorderLayout.CENTER);
+
+        revalidate();
+        repaint();
+    }
+
+    private JPanel setupSimpleButtons(ButtonListener buttonListener){
+        JPanel simpleButtons = new JPanel(new GridLayout(5, 1));
+
+        simpleButtons.add(new JLabel("Manual"));
+
+        insertCharacterButton = new JButton("Insert a Character");
+        insertCharacterButton.addActionListener(buttonListener);
+        simpleButtons.add(insertCharacterButton);
+
+        removeCharacterButton = new JButton("Remove Characters");
+        removeCharacterButton.addActionListener(buttonListener);
+        simpleButtons.add(removeCharacterButton);
+
+        replaceCharacterButton = new JButton("Replace a Character");
+        replaceCharacterButton.addActionListener(buttonListener);
+        simpleButtons.add(replaceCharacterButton);
+
+        swapCharactersButton = new JButton("Swap Two Characters");
+        swapCharactersButton.addActionListener(buttonListener);
+        simpleButtons.add(swapCharactersButton);
+
+        return simpleButtons;
+    }
+
+    public JPanel setupRandomButtons(ButtonListener buttonListener){
+        JPanel randomButtons = new JPanel(new GridLayout(5, 1));
+
+        randomButtons.add(new JLabel("Random"));
+
+        insertRandomCharacterButton = new JButton("Insert a Character");
+        insertRandomCharacterButton.addActionListener(buttonListener);
+        randomButtons.add(insertRandomCharacterButton);
+
+        removeRandomCharacterButton = new JButton("Remove a Character");
+        removeRandomCharacterButton.addActionListener(buttonListener);
+        randomButtons.add(removeRandomCharacterButton);
+
+        replaceRandomCharacterButton = new JButton("Replace a Character");
+        replaceRandomCharacterButton.addActionListener(buttonListener);
+        randomButtons.add(replaceRandomCharacterButton);
+
+        swapRandomCharactersButton = new JButton("Swap Two Characters");
+        swapRandomCharactersButton.addActionListener(buttonListener);
+        randomButtons.add(swapRandomCharactersButton);
+
+        return randomButtons;
+    }
+
+    public class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == insertCharacterButton){
+                BulletProofDialog<ScrambledMessage> dialog
+                        = new InsertionDialog(getParent(), message);
+
+                ScrambledMessage result = dialog.displayDialog();
+
+                if (result != null)
+                    message = result;
+            } else if (e.getSource() == removeCharacterButton){
+                BulletProofDialog<ScrambledMessage> dialog
+                        = new DeletionDialog(getParent(), message);
+
+                ScrambledMessage result = dialog.displayDialog();
+
+                if (result != null)
+                    message = result;
+            } else if (e.getSource() == replaceCharacterButton){
+                BulletProofDialog<ScrambledMessage> dialog
+                        = new ReplaceDialog(getParent(), message);
+
+                ScrambledMessage result = dialog.displayDialog();
+
+                if (result != null)
+                    message = result;
+            } else if (e.getSource() == swapCharactersButton){
+                BulletProofDialog<ScrambledMessage> dialog
+                        = new SwapDialog(getParent(), message);
+
+                ScrambledMessage result = dialog.displayDialog();
+
+                if (result != null)
+                    message = result;
+            } else if (e.getSource() == insertRandomCharacterButton){
+                message.insertRandomCharacter();
+            } else if (e.getSource() == removeRandomCharacterButton){
+                message.removeRandomCharacter();
+            } else if (e.getSource() == replaceRandomCharacterButton){
+                message.replaceRandomCharacter();
+            } else if (e.getSource() == swapRandomCharactersButton){
+                message.swapRandomCharacters();
+            }
+
+            updateCharacterGridPanel(message);
+        }
+    }
+}
